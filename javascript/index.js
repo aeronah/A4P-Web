f9887651cc75fe1d7b6b4dcd6744fab10a8c8877
aeronah <aeronah@hotmail.com>
2020-02-27 19:20:59 -0800

    Added bootstrap files

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+// Home page
+function onHomeStartBtnClick () {
+    location.href='start.html';
+}
+
+function onHomeControlBtnClick () {
+    location.href='control.html';
+}
+
+function onHomeConfigurationBtnClick () {
+    location.href='configuration.html';
+}
+
+// Start Page
+
+function onStartStopBtnClick() {
+    location.href='home.html';
+}
+
+//Control Page
+
+function onControlLaunchBtnClick() {
+    // Add MQTT communication
+}
+
+function onControlLeftBtnClick() {
+    // Add MQTT communication
+}
+
+function onControlRightBtnClick() {
+    // Add MQTT communication
+}
