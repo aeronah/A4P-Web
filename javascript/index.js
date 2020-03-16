b6e5651171747b737733dc6b23c33ccd7ff88b32
aeronah <aeronah@hotmail.com>
2020-03-16 14:53:38 -0700

    Removed alerts

--- javascript/index.js
+++ javascript/index.js
@@ -7,13 +7,13 @@ client.connect({
 });
 
 function onConnect() {
-  alert("connected");
-  client.subscribe("topic/WebTest");
+    document.getElementById("connection")="Connected";
+    client.subscribe("topic/WebTest");
 }
 
 function onFail() {
-  alert("failed");
-  client.connect(); // reconect if failed
+    document.getElementById("connection")="Failed";
+    client.connect(); // reconect if failed
 }
 
 function onReceive(msg) {
@@ -42,7 +42,6 @@ function onHomeConfigurationBtnClick () {
 
 function onStartStopBtnClick() {
     location.href='home.html';
-    alert("STOP");
     message = new Paho.MQTT.Message("STOP");
     message.destinationName = "topic/A4P";
     client.send(message);
@@ -55,21 +54,18 @@ function onControlBackBtnClick() {
 }
 
 function onControlLaunchBtnClick() {
-    alert("LAUNCH");
     message = new Paho.MQTT.Message("LAUNCH");
     message.destinationName = "topic/A4P";
     client.send(message);
 }
 
 function onControlLeftBtnClick() {
-    alert("STEPLEFT");
     message = new Paho.MQTT.Message("STEPLEFT");
     message.destinationName = "topic/A4P";
     client.send(message);
 }
 
 function onControlRightBtnClick() {
-    alert("STEPRIGHT");
     message = new Paho.MQTT.Message("STEPRIGHT");
     message.destinationName = "topic/A4P";
     client.send(message);
@@ -111,8 +107,6 @@ $('.dropdown-item').on('click',  function(){
 
 function onTrainingSaveBtnClick() {
     location.href='home.html';
-    alert(document.getElementById("number-passes").value);
-    alert(document.getElementById("difficulty").value);
     message = new Paho.MQTT.Message(document.getElementById("number-passes").value);
     message.destinationName = "topic/A4P/PPS";
     client.send(message);
@@ -127,35 +121,30 @@ function onDispenserBackBtnClick() {
 }
 
 function onDispenserRunBtnClick() {
-    alert("DRUN");
     message = new Paho.MQTT.Message("DRUN");
     message.destinationName = "topic/A4P/DISPENSER";
     client.send(message);
 }
 
 function onDispenserStepBtnClick() {
-    alert("DSTEP");
     message = new Paho.MQTT.Message("DSTEP");
     message.destinationName = "topic/A4P/DISPENSER";
     client.send(message);
 }
 
 function onDispenserCWBtnClick() {
-    alert("DCW");
     message = new Paho.MQTT.Message("DCW");
     message.destinationName = "topic/A4P/DISPENSER";
     client.send(message);
 }
 
 function onDispenserCCWBtnClick() {
-    alert("DCCW");
     message = new Paho.MQTT.Message("DCCW");
     message.destinationName = "topic/A4P/DISPENSER";
     client.send(message);
 }
 
 function onDispenserStopBtnClick() {
-    alert("DSTOP");
     message = new Paho.MQTT.Message("DSTOP");
     message.destinationName = "topic/A4P/DISPENSER";
     client.send(message);
@@ -167,35 +156,30 @@ function onRotationBackBtnClick() {
 }
 
 function onRotationRunBtnClick() {
-    alert("RRUN");
     message = new Paho.MQTT.Message("RRUN");
     message.destinationName = "topic/A4P/ROTATION";
     client.send(message);
 }
 
 function onRotationStepBtnClick() {
-    alert("RSTEP");
     message = new Paho.MQTT.Message("RSTEP");
     message.destinationName = "topic/A4P/ROTATION";
     client.send(message);
 }
 
 function onRotationCWBtnClick() {
-    alert("RCW");
     message = new Paho.MQTT.Message("RCW");
     message.destinationName = "topic/A4P/ROTATION";
     client.send(message);
 }
 
 function onRotationCCWBtnClick() {
-    alert("RCCW");
     message = new Paho.MQTT.Message("RCCW");
     message.destinationName = "topic/A4P/ROTATION";
     client.send(message);
 }
 
 function onRotationStopBtnClick() {
-    alert("RSTOP");
     message = new Paho.MQTT.Message("RSTOP");
     message.destinationName = "topic/A4P/ROTATION";
     client.send(message);
@@ -207,28 +191,24 @@ function onLauncherBackBtnClick() {
 }
 
 function onLauncherRunBtnClick() {
-    alert("LRUN");
     message = new Paho.MQTT.Message("LRUN");
     message.destinationName = "topic/A4P/LAUNCHER";
     client.send(message);
 }
 
 function onLauncherUpBtnClick() {
-    alert("LSU");
     message = new Paho.MQTT.Message("LSU");
     message.destinationName = "topic/A4P/LAUNCHER";
     client.send(message);
 }
 
 function onLauncherDownBtnClick() {
-    alert("LSD");
     message = new Paho.MQTT.Message("LSD");
     message.destinationName = "topic/A4P/LAUNCHER";
     client.send(message);
 }
 
 function onLauncherStopBtnClick() {
-    alert("LSTOP");
     message = new Paho.MQTT.Message("LSTOP");
     message.destinationName = "topic/A4P/LAUNCHER";
     client.send(message);
